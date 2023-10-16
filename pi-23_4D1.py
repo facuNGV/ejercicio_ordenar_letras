a6c526166cd44337e1adb92f85c113edf5743e74
Facuindo <facu_nicogas@outlook.com>
2023-10-16 14:25:58 -0300

    palabra en lista y caracteres de menor a mayor

---
+++ pi-23_4D1.py
@@ -0,0 +1,50 @@
+"""" 
+Realizar un programa que ordenes las letras de una palabra según el siguiente
+criterio:
+
+La nueva palabra se debe componer de
+
+Primero todas las letras minúsculas ordenadas de menor a mayor.
+Luego todas las letras mayúsculas ordenadas de menor a mayor.
+Todos los dígitos impares ordenados de menor a mayor.
+Todos los dígitos pares ordenados de menor a mayor.
+La palabra OrDenar1234 debería formar la siguiente nueva palabra según el
+criterior anterior: --> aenrrDO1324
+
+IMPORTANTE: Los métodos que le serán útil para determinar si un caracter es una
+letra minúscula, mayúscula o si es un dígito/número los debe investigar por su cuenta.
+"""
+print("Ordenar caracteres")
+objetivo = input("Ingrese palabra de entrada:\n")
+
+resultado = ""
+
+lista_objetivo = []
+pre_resultado = []
+lista_minusculas = []
+lista_mayusculas = []
+lista_impares = []
+lista_pares = []
+
+#convertir palabra en lista
+for i in objetivo:
+    lista_objetivo.append(i)
+
+#ordena caracteres de menor a mayor
+while len(lista_objetivo) != 0:
+  min_caracter = "}"
+  for i in lista_objetivo:
+    if i < min_caracter:
+      min_caracter = i
+  pre_resultado.append(min_caracter)
+  lista_objetivo.remove(min_caracter)
+
+
+
+
+
+
+
+
+
+
